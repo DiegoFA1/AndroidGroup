a0f6cb315c6b1ede593b4b01984af8e3c835859d
DiegoFA1 <144127274+DiegoFA1@users.noreply.github.com>
2023-11-13 15:35:01 -0500

    About Activity added, Added a toolbar and menu, for Main and About activity, for the rest of activities, we need to add the back_menu and a toolbar

--- app/src/main/java/georgebrown/group7/personalrestaurantguide/AboutActivity.java
+++ app/src/main/java/georgebrown/group7/personalrestaurantguide/AboutActivity.java
@@ -3,38 +3,36 @@ package georgebrown.group7.personalrestaurantguide;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class AboutActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar myToolbar = (Toolbar) findViewById(R.id.main_toolbar);
+        setContentView(R.layout.activity_about);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.about_toolbar);
         setSupportActionBar(myToolbar);
     }
 
-    // Menu
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.main_menu, menu);
+        inflater.inflate(R.menu.back_menu, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == R.id.about_button) {
-            Intent about_intent = new Intent(this,AboutActivity.class);
-            startActivity(about_intent);
+        if (item.getItemId() == R.id.back_button) {
+            finish();
             return true;
         }
         return false;
+
     }
 
 
