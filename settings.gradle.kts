603840dc293163ae848dfb7588775c19d589c90d
DiegoFA1 <144127274+DiegoFA1@users.noreply.github.com>
2023-11-13 14:48:46 -0500

    Unversioned Files

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Personal Restaurant Guide"
+include(":app")
+ 
