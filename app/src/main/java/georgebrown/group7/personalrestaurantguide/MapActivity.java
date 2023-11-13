a23da2e091d32a9c2f60288e05a9650c6a6a1109
DiegoFA1 <144127274+DiegoFA1@users.noreply.github.com>
2023-11-13 18:57:02 -0500

    Main done, added other activities classes

---
+++ app/src/main/java/georgebrown/group7/personalrestaurantguide/MapActivity.java
@@ -0,0 +1,14 @@
+package georgebrown.group7.personalrestaurantguide;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MapActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+    }
+}
