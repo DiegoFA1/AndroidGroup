d841d96941403824a5ae4b62a6fcea36f5eff0f8
chloe <chloe@DESKTOP-SI98GST>
2023-12-02 20:25:37 -0500

    modify AddEditActivity

--- app/src/main/java/georgebrown/group7/personalrestaurantguide/AddEditActivity.java
+++ app/src/main/java/georgebrown/group7/personalrestaurantguide/AddEditActivity.java
@@ -1,21 +1,62 @@
 package georgebrown.group7.personalrestaurantguide;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RatingBar;
 
 public class AddEditActivity extends AppCompatActivity {
+    private EditText editTextRestaurantName, editTextRestaurantAddress, editTextPhone,
+            editTextRestaurantDescription, editTextTags;
+    private RatingBar ratingBar;
+    private Button buttonSave;
+    private DbManager dbManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_edit);
-        Toolbar myToolbar = (Toolbar) findViewById(R.id.add_toolbar);
+
+        // Initialize views
+        editTextRestaurantName = findViewById(R.id.editTextRestaurantName);
+        editTextRestaurantAddress = findViewById(R.id.editTextRestaurantAddress);
+        editTextPhone = findViewById(R.id.editTextPhone);
+        editTextRestaurantDescription = findViewById(R.id.editTextRestaurantDescription);
+        editTextTags = findViewById(R.id.editTextTags);
+        ratingBar = findViewById(R.id.ratingBar);
+        buttonSave = findViewById(R.id.buttonSave);
+
+        // Set click listener for save button
+        buttonSave.setOnClickListener(v -> onSaveButtonClicked());
+
+        // Set up toolbar
+        Toolbar myToolbar = findViewById(R.id.add_toolbar);
         setSupportActionBar(myToolbar);
+    }
+
+    private void onSaveButtonClicked() {
+        // Get data from views
+        String restaurantName = editTextRestaurantName.getText().toString().trim();
+        String restaurantAddress = editTextRestaurantAddress.getText().toString().trim();
+        String phone = editTextPhone.getText().toString().trim();
+        String description = editTextRestaurantDescription.getText().toString().trim();
+        String tags = editTextTags.getText().toString().trim();
+        float rating = ratingBar.getRating();
+        // Insert data into the database
+        long id = dbManager.insert(restaurantName, description, restaurantAddress, phone, tags, rating);
+
+        // Pass the new restaurant data back to the calling activity
+//        Intent resultIntent = new Intent();
+//        resultIntent.putExtra("newRestaurant", newRestaurant);
+//        setResult(RESULT_OK, resultIntent);
+
+        // Finish the activity
+        finish();
     }
 
     @Override
@@ -32,6 +73,5 @@ public class AddEditActivity extends AppCompatActivity {
             return true;
         }
         return false;
-
     }
 }
